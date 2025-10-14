1d6bf2f36dc1cf9ef0e0b9fb24f571fc38cf9b54
michaelgirgis1 <engmichaelgerges@gmail.com>
2025-10-14 10:30:28 +0300

    enhance breadcrumb change form inputs style

--- projects/demo-app/src/app/side-bar/side-bar.ts
+++ projects/demo-app/src/app/side-bar/side-bar.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { InputComponent } from './../../../../ui-components-lib/src/lib/form-components/components/input/input.component';
+import { FormUtils } from '@corp-products/ui-components';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [],
-  templateUrl: './side-bar.html',
+  imports: [InputComponent],
+templateUrl: './side-bar.html',
   styleUrl: './side-bar.scss'
 })
 export class SideBar {
+  form: FormGroup = new FormGroup({
+    search: new FormControl(''),
+  });
+  getFormControl = FormUtils.getFormControl;
 
 }
