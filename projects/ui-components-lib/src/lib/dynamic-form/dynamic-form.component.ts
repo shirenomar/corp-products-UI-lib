1facc6ccab76a6a0ce308b56d864c71bb0c1591b
melsafty <melsafty.c@stc.com.sa>
2025-11-04 15:21:22 +0300

    feat(dynamic-form): enhance dynamic form functionality with new input types and validation - Added support for 'date' and 'email' input types in dynamic forms. - Implemented custom validation rules for new input types. - Updated documentation and examples to reflect new features.

--- projects/ui-components-lib/src/lib/dynamic-form/dynamic-form.component.ts
+++ projects/ui-components-lib/src/lib/dynamic-form/dynamic-form.component.ts
@@ -1,19 +1,45 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormGroup, ReactiveFormsModule } from "@angular/forms";
-import { DynamicFormData, FormFieldTypeEnum, InputsMap } from "./dynamic-form.interface";
-import {DatePickerComponent, FormUtils, SelectButtonComponent, ValidationErrorsPipe} from "../form-components";
-import { TranslateModule } from "@ngx-translate/core";
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import {
+  AutoCompleteComponent,
+  DatePickerComponent,
+  FormUtils,
+  InputComponent,
+  SelectButtonComponent,
+  SelectComponent,
+  SwitchComponent,
+  ValidationErrorsPipe,
+} from '../form-components';
+import { DynamicFormData, FormFieldTypeEnum, InputsMap } from './dynamic-form.interface';
 
 @Component({
-  selector: "app-dynamic-form",
+  selector: 'app-dynamic-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, DatePickerComponent, ValidationErrorsPipe, TranslateModule, SelectButtonComponent],
-  templateUrl: "./dynamic-form.component.html",
-  styleUrl: "./dynamic-form.component.scss"
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    DatePickerComponent,
+    ValidationErrorsPipe,
+    TranslateModule,
+    SelectButtonComponent,
+    InputComponent,
+    SelectComponent,
+    AutoCompleteComponent,
+    SwitchComponent,
+  ],
+  templateUrl: './dynamic-form.component.html',
+  styleUrl: './dynamic-form.component.scss',
 })
 export class DynamicFormComponent implements OnInit {
   @Input({ required: true }) dynamicFormData: DynamicFormData;
+  // Generic field change outputs (optional for consumers)
+  @Output() selectButtonChange = new EventEmitter<{ name: string; value: any }>();
+  @Output() selectChange = new EventEmitter<{ name: string; event: any }>();
+  @Output() switchChange = new EventEmitter<{ name: string; value: boolean }>();
+  @Output() autoCompleteSearch = new EventEmitter<{ name: string; query: string }>();
+  @Output() autoCompleteSelect = new EventEmitter<{ name: string; event: any }>();
   inputsNames: string[] = [];
   formGroup: FormGroup;
   inputsMap: InputsMap;
@@ -26,4 +52,9 @@ export class DynamicFormComponent implements OnInit {
     this.inputsNames = Object.keys(this.inputsMap || {});
   }
 
+  onSwitchChange(name: string, event: any) {
+    const value = typeof event === 'boolean' ? event : event === 'true';
+    this.getFormControl(name, this.formGroup).setValue(value);
+    this.switchChange.emit({ name, value });
+  }
 }
