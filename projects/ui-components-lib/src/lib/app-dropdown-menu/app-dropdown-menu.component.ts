8e37491f1be4c2e9c0d430f8d8fe15a00ae264a2
Aya Abdelmalek <ayamalekhasan@gmail.com>
2026-01-25 13:31:10 +0200

    fix error

--- projects/ui-components-lib/src/lib/app-dropdown-menu/app-dropdown-menu.component.ts
+++ projects/ui-components-lib/src/lib/app-dropdown-menu/app-dropdown-menu.component.ts
@@ -17,7 +17,7 @@ import { Button } from 'primeng/button';
   styleUrl: "./app-dropdown-menu.component.scss",
   encapsulation: ViewEncapsulation.None
 })
-export class AppDropdownMenuComponent extends Button implements OnInit {
+export class AppDropdownMenuComponent implements OnInit {
   @Input({ required: true }) buttonTitle: string;
   @Input({ required: true }) menuItems: DropdownMenuItem[] = [];
   @Input() popupMenuStyle: "white" | "purple" = "purple";
@@ -29,9 +29,7 @@ export class AppDropdownMenuComponent extends Button implements OnInit {
   router = inject(Router);
 
   showMenu: boolean;
-
-   override ngOnInit() {
-    super.ngOnInit();
+  ngOnInit() {
     this.showMenu = this.menuItems.some((item) => {
       return item.show;
     });
