745b7258cef54c1bb3d244c2599fc8ca0466bd34
mohamedismael960 <59924980+mohamedismael960@users.noreply.github.com>
2026-04-21 15:38:21 +0300

    Bug/autocomplete 2054 (#118)
    * fix(ECMS-2054): System not indicate which wrong mail when add more email
    
    * make allowedDomains dynamic
    
    * chnage emailDomain to email
    
    * fix allowedDomains
    
    ---------
    
    Co-authored-by: Mohamed Ismael Abdelhady <mohabdelhady.c@stc.com.sa>

--- projects/ui-components-lib/src/lib/form-components/@utils/validations/email-stc.validator.ts
+++ projects/ui-components-lib/src/lib/form-components/@utils/validations/email-stc.validator.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 /**
  * Validator function to check if the control value is a valid email address.
