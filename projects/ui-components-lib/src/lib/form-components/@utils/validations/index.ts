ebe9fcf9b79a2aeeeb7fb42c92317903cc63b886
Mohamed Elsafty <35928436+mohamedelsafty@users.noreply.github.com>
2026-04-22 17:43:39 +0200

    feat(validations): add max repeated characters validator and integrat… (#122)
    * feat(validations): add max repeated characters validator and integrate into form controls
    
    * feat(validations): enhance validation messages and add max repeated characters error handling
    
    ---------
    
    Co-authored-by: melsafty <melsafty.c@stc.com.sa>

--- projects/ui-components-lib/src/lib/form-components/@utils/validations/index.ts
+++ projects/ui-components-lib/src/lib/form-components/@utils/validations/index.ts
@@ -2,5 +2,6 @@ export * from './email-stc.validator';
 export * from './error-keys.enum';
 export * from './form-validation.service';
 export * from './numbers-only.validator';
+export * from './repeated-chars.validator';
 export * from './saudi-phone.validator';
 export * from './validation-message.pipe';
