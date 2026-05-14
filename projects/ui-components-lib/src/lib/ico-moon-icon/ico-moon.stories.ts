06fd9e9b467e287425192e12699d8566764e0eca
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2026-05-14 04:33:04 +0300

    refactor(ico-moon-icon): remove unused CommonModule import from IcoMoonIconComponent stories

--- projects/ui-components-lib/src/lib/ico-moon-icon/ico-moon.stories.ts
+++ projects/ui-components-lib/src/lib/ico-moon-icon/ico-moon.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { moduleMetadata } from '@storybook/angular';
-import { CommonModule } from '@angular/common';
 import { IcoMoonIconComponent } from './ico-moon-icon.component';
 
 
